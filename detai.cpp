bcc555b5f5d06201723295d3190b16c58070fe12
ZeroIHC <188943239+zeroihc517@users.noreply.github.com>
2026-09-13 10:23:38 +0700

    Update detai.cpp

--- detai.cpp
+++ detai.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
 class XXXX {
+protected:
+  
+public:
+  
+};
+class KeThua1 : public XXXX {
 
 };
+
+class KeThua2 : public XXXX {
+
+};
+
+class KeThua3 : public XXXX {
+
+};
+
+class HienThiThongTin() {
+
+};
+
 int main() {
 
   return 0;
